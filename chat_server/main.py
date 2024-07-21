261221bb5303c2d9a7e51d29f587e132b90856c0
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2024-07-21 21:58:52 +0900

    chore: Update chat server to remove debug mode and use default port for Flask app

--- chat_server/main.py
+++ chat_server/main.py
@@ -41,4 +41,5 @@ def chat():
     return str(result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0' ,port=6001)
+    #app.run(debug=True, host='0.0.0.0' ,port=6001)
+    app.run()
